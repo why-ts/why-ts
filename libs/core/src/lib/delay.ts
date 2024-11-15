2509138d6ff4f628a99b2b2422546df4443fecf4
Kevin Leung <kevinresol@gmail.com>
2024-11-15 11:19:45 +1100

    fix(core): non default export

--- libs/core/src/lib/delay.ts
+++ libs/core/src/lib/delay.ts
@@ -1,5 +1,5 @@
-export default function delay(ms: number): Promise<void>;
-export default function delay<T>(ms: number, get: () => T): Promise<T>;
-export default function delay<T>(ms: number, get?: () => T): Promise<T> {
-	return new Promise((resolve) => setTimeout(() => resolve(get?.() as T), ms));
+export function delay(ms: number): Promise<void>;
+export function delay<T>(ms: number, get: () => T): Promise<T>;
+export function delay<T>(ms: number, get?: () => T): Promise<T> {
+  return new Promise((resolve) => setTimeout(() => resolve(get?.() as T), ms));
 }
